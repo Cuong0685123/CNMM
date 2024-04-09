c2efdfb39e222d176f26a1c882ddedf6a5ef3829
Cuong0685123 <eticon0685@gmail.com>
2024-04-09 14:17:21 +0700

    lan2

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["http://localhost:5000"],
 		methods: ["GET", "POST"],
 	},
 });
@@ -21,7 +21,7 @@ const userSocketMap = {}; // {userId: socketId}
 io.on("connection", (socket) => {
 	console.log("a user connected", socket.id);
 
-	const userId = socket.handshake.query.userId;
+	let userId = socket.handshake.query.userId;
 	if (userId != "undefined") userSocketMap[userId] = socket.id;
 
 	// io.emit() is used to send events to all the connected clients
